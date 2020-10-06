b8f27a57f8afa99d8d3dad72bf7360ad48955942
OliiynykDima <d.oliynyk2007kiev@gmail.com>
2020-10-07 02:12:23 +0300

    v1

---
+++ task-5.js
@@ -0,0 +1,19 @@
+const checkForSpam = function (message) {
+    if(message.toLowerCase().indexOf('spam') != -1 || message.toLowerCase().indexOf('sale') != -1){
+        return true;
+    }
+    else{
+        return false;
+    }
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(checkForSpam('Latest technology news')); // false
+
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
+
+console.log(checkForSpam('Get best sale offers now!')); // true
+
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
---
+++ task-6.js
@@ -0,0 +1,9 @@
+let total = 0;
+let input = +prompt('Введите любое число');
+for(let i = 0; i = input; i++){
+	total += input;
+	input = +prompt('Введите любое число');
+};
+alert(`Общая сумма чисел равна ${+total}`);
+
+console.log(Boolean("false"));
